510c297f937aa6df8378e684f45f24f57d6cf9a6
Niklas Larsson <niklas@mm.st>
2016-04-03 22:44:52 +0200

    Don't use hungarian prefix, import the others qualified instead

--- src/Idris/IBCTypes.hs
+++ src/Idris/IBCTypes.hs
@@ -1,7 +1,7 @@
 module IBCTypes where
 
-import Idris.Core.TT
-import Idris.AbsSyntaxTree
+import qualified Idris.Core.TT as T
+import qualified Idris.AbsSyntaxTree as A
 
 type NameRef = Int
 type TermRef = Int
@@ -9,81 +9,83 @@ type TextRef = Int
 type PTermRef = Int
 
 data FBinder =
-    FLam TermRef |
-    FPi (Maybe ImplicitInfo) TermRef TermRef |
-    FLet TermRef TermRef |
-    FNLet TermRef TermRef |
-    FHole TermRef |
-    FGHole Int [NameRef] TermRef |
-    FGuess TermRef TermRef |
-    FPVar TermRef |
-    FPVTy TermRef
+    Lam TermRef |
+    Pi (Maybe ImplicitInfo) TermRef TermRef |
+    Let TermRef TermRef |
+    NLet TermRef TermRef |
+    Hole TermRef |
+    GHole Int [NameRef] TermRef |
+    Guess TermRef TermRef |
+    PVar TermRef |
+    PVTy TermRef
     deriving (Show, Eq, Ord)
 
 
 data FTerm =
-    FP NameType NameRef TermRef |
-    FV Int |
-    FBind NameRef FBinder TermRef |
-    FApp FAppStatus TermRef TermRef |
-    FConstant Const |
-    FProj TermRef Int |
-    FErased |
-    FImpossible |
-    FTType UExp |
-    FUType Universe
+    P NameType NameRef TermRef |
+    V Int |
+    Bind NameRef FBinder TermRef |
+    App FAppStatus TermRef TermRef |
+    Constant Const |
+    Proj TermRef Int |
+    Erased |
+    Impossible |
+    TType UExp |
+    UType Universe
     deriving (Show, Eq, Ord)
 
 data FAppStatus =
-    FComplete |
-    FMaybeHoles |
-    FHoles [NameRef]
+    Complete |
+    MaybeHoles |
+    Holes [NameRef]
     deriving (Show, Eq, Ord)
 
+type FFC = Maybe (Word64, Word64)
+
 data FPTerm =
-    FPQuote Raw |
-    FPRef FFC [FFC] NameRef |
-    FPInferDef FFC [FFC] NameRef |
-    FPPatvar FFC NameRef |
-    FPLam FFC NameRef FFC PTermRef PTermRef |
-    FPPi Plicity NameRef FFC PTermRef PTermRef |
-    FPLet FFC NameRef FFC PTermRef PTermRef PTermRef |
-    FPTyped PTermRef PTermRef |
-    FPApp FFC PTermRef [FPArg] |
-    FPWithApp FFC PTermRef PTermRef |
-    FPAppImpl PTermRef [ImplicitInfo] |
-    FPAppBind FFC PTermRef [FPArg] |
-    FPMatchApp FFC NameRef |
-    FPIfThenElse FFC PTermRef PTermRef PTermRef |
-    FPCase FFC PTermRef [(PTermRef, PTermRef)] |
-    FPTrue FFC PunInfo |
-    FPResolveTC FFC |
-    FPRewrite FFC PTermRef PTermRef (Maybe PTermRef) |
-    FPPair FFC [FFC] PunInfo PTermRef PTermRef |
-    FPDPair FFC [FFC] PunInfo PTermRef PTermRef PTermRef |
-    FPAs FFC NameRef PTermRef |
-    FPAlternative [(NameRef, NameRef)] PAltType [PTermRef] |
-    FPHidden PTermRef |
-    FPType FFC |
-    FPUniverse Universe |
-    FPGoal FFC PTermRef NameRef PTermRef |
-    FPConstant FFC Const |
-    FPlaceHolder |
-    FPDoBlock [FPDo] |
-    FPIdiom FFC PTermRef |
-    FPReturn FFC |
-    FPMetavar FFC NameRef |
-    FPProof [FPTactic] |
-    FPTactics [FPTActic] |
-    FPElabError Err |
-    FPImpossible |
-    FPCoerced PTermRef |
-    FPDisamb [[TextRef]] PTermRef |
-    FPUnifyLog PTermRef |
-    FPNoImplicits PTermRef |
-    FPQuasiquote PTermRef (Maybe PTermRef) |
-    FPUnquote PTermRef |
-    FPQuoteName NameRef Bool FFC |
-    FPRunElab FFC PTermRef [TextRef] |
-    FPConstSugar FFC PTermRef
+    PQuote Raw |
+    PRef FFC [FFC] NameRef |
+    PInferDef FFC [FFC] NameRef |
+    PPatvar FFC NameRef |
+    PLam FFC NameRef FFC PTermRef PTermRef |
+    PPi Plicity NameRef FFC PTermRef PTermRef |
+    PLet FFC NameRef FFC PTermRef PTermRef PTermRef |
+    PTyped PTermRef PTermRef |
+    PApp FFC PTermRef [FPArg] |
+    PWithApp FFC PTermRef PTermRef |
+    PAppImpl PTermRef [ImplicitInfo] |
+    PAppBind FFC PTermRef [FPArg] |
+    PMatchApp FFC NameRef |
+    PIfThenElse FFC PTermRef PTermRef PTermRef |
+    PCase FFC PTermRef [(PTermRef, PTermRef)] |
+    PTrue FFC PunInfo |
+    PResolveTC FFC |
+    PRewrite FFC PTermRef PTermRef (Maybe PTermRef) |
+    PPair FFC [FFC] PunInfo PTermRef PTermRef |
+    PDPair FFC [FFC] PunInfo PTermRef PTermRef PTermRef |
+    PAs FFC NameRef PTermRef |
+    PAlternative [(NameRef, NameRef)] PAltType [PTermRef] |
+    PHidden PTermRef |
+    PType FFC |
+    PUniverse Universe |
+    PGoal FFC PTermRef NameRef PTermRef |
+    PConstant FFC Const |
+    PlaceHolder |
+    PDoBlock [FPDo] |
+    PIdiom FFC PTermRef |
+    PReturn FFC |
+    PMetavar FFC NameRef |
+    PProof [FPTactic] |
+    PTactics [FPTActic] |
+    PElabError Err |
+    PImpossible |
+    PCoerced PTermRef |
+    PDisamb [[TextRef]] PTermRef |
+    PUnifyLog PTermRef |
+    PNoImplicits PTermRef |
+    PQuasiquote PTermRef (Maybe PTermRef) |
+    PUnquote PTermRef |
+    PQuoteName NameRef Bool FFC |
+    PRunElab FFC PTermRef [TextRef] |
+    PConstSugar FFC PTermRef
     deriving (Eq, Show, Ord)
